6de04bd7657b13555db05008df2b52430cb978c5
arthur <arthur.rios007@gmail.com>
2024-04-19 10:04:40 -0300

    chore: Add github auth config

--- src/app/(home)/explore/page.tsx
+++ src/app/(home)/explore/page.tsx
@@ -1,25 +1,41 @@
+'use client'
+
 import { SmallBookCard } from '@/app/components/small-book-card'
 import { Tag } from '@/app/components/tag'
+import { useState } from 'react'
 
 export default function Explore() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
+
+  function handleChangeFilter(selectedCategory: string) {
+    if (selectedCategories.includes(selectedCategory)) {
+      setSelectedCategories((prevState) =>
+        prevState.filter((category) => category !== selectedCategory),
+      )
+    } else {
+      setSelectedCategories((prevState) => [...prevState, selectedCategory])
+    }
+  }
+
   return (
     <div className="pb-12 pl-18 pr-24 pt-3">
       <div className="h-full min-w-explore">
         <div className="mb-12 flex flex-wrap items-center gap-3">
-          <Tag title="All" />
-          <Tag title="Programming" isSelected />
-          <Tag title="Education" />
-          <Tag title="Fantasy" />
-          <Tag title="Fiction" isSelected />
-          <Tag title="Horror" isSelected />
-          <Tag title="Education" />
-          <Tag title="Fantasy" />
-          <Tag title="Fiction" />
-          <Tag title="Horror" isSelected />
-          <Tag title="Education" />
-          <Tag title="Fantasy" />
-          <Tag title="Fiction" isSelected />
-          <Tag title="Horror" />
+          <Tag
+            title="All"
+            isSelected={selectedCategories.length === 0}
+            onClick={() => setSelectedCategories([])}
+          />
+          {Array.from({ length: 12 }).map((_, i) => {
+            return (
+              <Tag
+                key={i}
+                title="Programming"
+                isSelected={selectedCategories.includes(String(i))}
+                onClick={() => handleChangeFilter(String(i))}
+              />
+            )
+          })}
         </div>
         <div className="grid grid-cols-3 gap-5 2xl:grid-cols-4">
           {Array.from({ length: 22 }).map((_, i) => {
