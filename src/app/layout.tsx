f0915d2fb463e5c832eff134a9d83cb689b0f0c4
arthur <arthur.rios007@gmail.com>
2024-04-26 10:55:39 -0300

    chore: Add tanstack query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import SessionProvider from '@/app/components/session-provider'
 
 import './globals.css'
 import { auth } from '@/lib/auth'
+import { QueryClient } from './components/query-client-provider'
 
 const nunitoSans = Nunito_Sans({
   subsets: ['latin'],
@@ -26,7 +27,9 @@ export default async function RootLayout({
   return (
     <html className={nunitoSans.className} lang="en">
       <body className="bg-gray-800 text-gray-100 antialiased">
-        <SessionProvider session={session}>{children}</SessionProvider>
+        <QueryClient>
+          <SessionProvider session={session}>{children}</SessionProvider>
+        </QueryClient>
       </body>
     </html>
   )
